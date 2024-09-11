5b957879c40eb06ae7e25c7ed1c3af055b35b3b1
gorin <gorin333roman@yandex.ru>
2024-09-11 20:45:09 +0300

    имя

--- src/card.py
+++ src/card.py
@@ -1,1 +1,23 @@
+"""Карты Зелень"""
+from typing import Self
+
+class Card:
+    VEGETABLES = ['t', 'ca', 'co', 'b', 'e']
+    NUMBERS = list(range(4))
+
+    def __init__(self, vegs: str, numb: int):
+        if vegs not in Card.VEGETABLES:
+            raise ValueError
+        if numb not in Card.NUMBERS:
+            raise ValueError
+
+        self.vegs = vegs
+        self.numb = numb
+
+    def __repr__(self):
+        return f'{self.vegs}{self.numb}'
+
+
+
+
 
