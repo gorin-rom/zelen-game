16386db61cda3aa0d26354220a42a77b82df5eaf
gorin <gorin333roman@yandex.ru>
2024-10-28 01:20:02 +0300

    тесты, кроме score

--- test/test_card.py
+++ test/test_card.py
@@ -3,6 +3,7 @@ import pytest
 from src.card import Card
 from src.price import VegBox
 
+
 def test_init():
     c = Card(М=2, Т=1)
     assert c.М == 2
@@ -20,33 +21,45 @@ def test_save():
     assert repr(c) == 'ММК'
     assert c.save() == 'ММК'
 
+
 def test_load():
     m = 'ММК'
+    t = 'ТТТ'
+    z = 'ЗЗМ'
+    d = Card.load(t)
     c = Card.load(m)
+    q = Card.load(z)
     assert c == Card(М=2, К=1)
+    assert d == Card(Т=3)
+    assert q == Card(З=2, М=1)
+
 
 def test_eq():
     c1 = Card(Т=3)
     c2 = Card(Т=3)
     c3 = Card(Б=1, З=2)
+    c4 = Card(К=2, М=1)
     assert c1 == c2
     assert c1 != c3
     assert c2 != c3
+    assert c1 != c4
+
 
 def test_score():
     q = Card(М=2, З=1)
-    p = VegBox(1, 4, 2, 3, 5)
-    assert q.score(p) == 4 * 2 + 5
+    p = VegBox(Т=1, М=4, К=2, Б=3, З=5)
+    assert p.score(q) == 4 * 2 + 5
+
 
 def test_all_cards():
-    cards = Card.all_cards(['Т', 'М', 'К', 'Б', 'З'])
+    cards = Card.all_cards(['Т', 'М'])
     expected_cards = [
-        Card.load('БММ'),
-        Card.load('ББК'),
-        Card.load('БББ'),
         Card.load('ТТМ'),
-        Card.load('ЗЗТ'),
-        Card.load('ККБ')
+        Card.load('ТММ'),
+        Card.load('ТТТ'),
+        Card.load('ТТМ'),
+        Card.load('ТММ'),
+        Card.load('МММ')
     ]
     assert cards == expected_cards
 
