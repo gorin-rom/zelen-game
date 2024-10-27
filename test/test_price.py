78b2e2c00722d360eb7e138ca31b2e89bb5bf119
gorin <gorin333roman@yandex.ru>
2024-10-28 00:56:01 +0300

    тесты, кроме add

--- test/test_price.py
+++ test/test_price.py
@@ -1,19 +1,29 @@
 import pytest
 
-from src.price import Price
+from  src.card import Card
+from src.price import VegBox
 
 def test_init():
-    p = Price(2, 3, 1, 4, 0)
+    p = VegBox(Т=2, М=3, К=1, Б=4, З=0)
     assert p.Т == 2
     assert p.М == 3
     assert p.К == 1
     assert p.Б == 4
     assert p.З != 4
 
+def test_valid():
+    with pytest.raises(ValueError):
+        n = VegBox(Т=100)
+
 def test_save():
-    p = Price(4, 2, 3, 1, 0)
-    assert repr(p) == '42310'
+    p = VegBox(Т=4, М=2, К=3, Б=1, З=0)
+    assert repr(p) == 'Т:4 М:2 К:3 Б:1 З:0 '
 
-
-
+def test_add():
+    v = VegBox(Т=0, М=1, К=2, Б=3, З=4)
+    v.add('КББ')
+    assert v.Т == 0
+    assert v.М == 1
+    assert v.К == 3
+    assert v.Б == 5
 
