e9523cc9bc44f77ea9261770879fbbbd73b3ee60
gorin <gorin333roman@yandex.ru>
2024-11-11 03:34:46 +0300

    score работает

--- src/hand.py
+++ src/hand.py
@@ -4,12 +4,10 @@ from src.card import Card
 
 
 class Hand:
-    def __init__(self, cards:  list[Card] | None = None):
+    def __init__(self, cards: list[Card] | None = None):
         if cards is None:
             cards = []
         self.cards: list[Card] = cards
-        '''for v in Card.VEGETABLES:
-            setattr(self, v, kwargs.get(v, 0))'''
 
     def __repr__(self):
         return self.save()
@@ -24,19 +22,21 @@ class Hand:
             other = Hand.load(other)
         return self.cards == other.cards
 
-    def save(self) -> str:
-        scards = [c.save() for c in self.cards]
-        s = ' '.join(scards)
-        return s
-
     @classmethod
-    def load(cls, text: str) -> typing.Self:
+    def load(cls, text: str) -> 'Hand':
         cards = [Card.load(s) for s in text.split()]
         return cls(cards=cards)
 
     def add_card(self, card: Card):
         self.cards.append(card)
 
+    def __getattr__(self, name):
+        if name in Card.VEGETABLES:
+            return sum(getattr(card, name) for card in self.cards)
+        raise AttributeError
+
     def score(self, other):
-        return self.veg * other.veg
-     #   return Card.score
+        return sum(getattr(self, v) * getattr(other, v) for v in Card.VEGETABLES)
+
+
+    #   return Card.score
