e4f4c74aa7786668fb715bb9c349bf5f4f27e863
gorin <gorin333roman@yandex.ru>
2024-11-26 02:30:36 +0300

    test gamestate

--- test/test_game_state.py
+++ test/test_game_state.py
@@ -7,44 +7,47 @@ from src.player import Player
 from src.price import VegBox
 
 data = {
-    'Price': 'Т:2 М:1 К:4 Б:0 З:3',
-    'Deck': "ТТТ ТММ ТТМ МММ ТММ ТТМ",
-    'Round': 1,
-    'current_player_index': 1,
-    'players': [
-        {'name': 'Leo', 'hand': 'Т:3 М:1 К:0 Б:4 З:2', 'score': 6},
-        {'name': 'Mike', 'hand': 'Т:3 М:4 К:1 Б:0 З:2', 'score': 10}]}
+    "Price": 'Т:2 М:1 К:4 Б:0 З:3',
+    "Deck": 'ТТМ ТММ ТТТ ТТМ ТММ МММ',
+    "Round": 1,
+    "current_player_index": 1,
+    "cards": ['ЗЗЗ', 'БЗЗ', 'ББЗ'],
+    "players": [
+        {'name': 'Leo', 'hand': 'ТТТ МММ ТММ', 'score': 6},
+        {'name': 'Mike', 'hand': 'ТТМ ТММ ТТМ', 'score': 10}]}
 
-Leo = Player.load(data['players'][0])
-Mike = Player.load(data['players'][1])
+Leo = Player.load(data["players"][0])
+Mike = Player.load(data["players"][1])
+cards = (Card.load(s) for s in data["cards"])
 full_deck = Deck(None)
 price = VegBox.load('Т:2 М:1 К:4 Б:0 З:3')
 
 
 def test_init():
     players = [Leo, Mike]
-    game = GameState(players=players, deck=full_deck, price=price, current_player=0, round=1)
+    game = GameState(players=players, deck=full_deck, price=price, cards=cards, current_player=0, iround=1)
     assert game.players == players
     assert game.deck == full_deck
     assert game.price == price
+    assert game.cards == cards
     assert game._current_player == 0
-    assert game.round == 1
+    assert game.iround == 1
 
 
 def test_current_players():
     players = [Leo, Mike]
-    game = GameState(players=players, deck=full_deck, price=price, round=1)
-    assert game._current_player == Leo
-    game = GameState(players=players, deck=full_deck, price=price, current_player=1, round=1)
+    game = GameState(players=players, deck=full_deck, price=price, cards=cards, iround=1)
+    assert game.current_player() == Leo
+    game = GameState(players=players, deck=full_deck, price=price, cards=cards, current_player=1, iround=1)
     assert game.current_player() == Mike
 
 
 def test_eq():
     players = [Leo, Mike]
-    game1 = GameState(players=players, deck=full_deck, price=price, current_player=1, round=1)
-    game2 = GameState(players=players.copy(), deck=full_deck, price=price, current_player=1, round=1)
+    game1 = GameState(players=players, deck=full_deck, price=price, cards=cards, current_player=1, iround=1)
+    game2 = GameState(players=players.copy(), deck=full_deck, price=price, cards=cards, current_player=1, iround=1)
     game3 = GameState(
-        players=players, deck=Deck(Card.all_cards(['Т', 'М'])), price=price, current_player=1, round=1
+        players=players, deck=Deck(Card.all_cards(['Б', 'З'])), price=price, cards=cards, current_player=1, iround=1
     )
     assert game1 == game2
     assert game1 != game3
@@ -52,7 +55,7 @@ def test_eq():
 
 def test_save():
     players = [Leo, Mike]
-    game = GameState(players=players, deck=Deck(Card.all_cards(['Т', 'М'])), price=price, current_player=1, round=1)
+    game = GameState(players=players, deck=Deck(Card.all_cards(['Т', 'М'])), price=price, cards=cards, current_player=1, iround=1)
     assert game.save() == data
 
 
@@ -63,8 +66,8 @@ def test_load():
 
 def test_next_player():
     game = GameState.load(data)
-    assert str(game.current_player()) == str(Mike)
+    assert str(game.current_player()) == 'Mike(10): ТТМ ТММ ТТМ'
     game.next_player()
-    assert str(game.current_player()) == str(Leo)
+    assert str(game.current_player()) == 'Leo(6): ТТТ МММ ТММ'
     game.next_player()
-    assert str(game.current_player()) == str(Mike)
+    assert str(game.current_player()) == 'Mike(10): ТТМ ТММ ТТМ'
