4adffc18407b98218f0364ec3277d43e9d8352f4
gorin <gorin333roman@yandex.ru>
2024-11-26 04:33:19 +0300

    в процессе

--- src/gameserver.py
+++ src/gameserver.py
@@ -10,8 +10,10 @@ from src.price import VegBox
 
 
 class GamePhase(enum.StrEnum):
+    BEGIN_ROUND = "Deal cards"
     CHOOSE_CARD = "Choose card"
     NEXT_PLAYER = "Switch current player"
+    END_ROUND = "Price change"
     DECLARE_WINNER = "Declare a winner"
     GAME_END = "Game ended"
 
@@ -21,37 +23,53 @@ class GameServer:
         self.gamestate = gamestate
         self.player_types = player_types
 
+    @classmethod
+    def get_players(cls):
+        player_count = cls.request_player_count()
 
-    def new_game(self):
-        player_count = self.request_player_count()
         player_types = {}
-        for p in range(player_count):
-            name, kind = self.request_player_count()
+        for _ in range(player_count):
+            name, kind = cls.request_player()
             player = Player(name, Hand())
             player_types[player] = kind
+        return player_types
 
-        deck = Deck(cards=[Card(veg) for veg in Card.VEGETABLES])
-        self.gamestate = GameState(list(player_types.keys()), deck)
+    def new_game(self, player_types: dict):
+        deck = Deck(cards=None)
+        price = VegBox()
+        self.gamestate = GameState(list(player_types.keys()), deck, price)
 
     def run(self):
         current_phase = GamePhase.CHOOSE_CARD
         while current_phase != GamePhase.GAME_END:
             phases = {
+                GamePhase.BEGIN_ROUND: self.new_game,
                 GamePhase.CHOOSE_CARD: self.choose_card_phase,
                 GamePhase.NEXT_PLAYER: self.next_player_phase,
+                GamePhase.END_ROUND: self.end_round_phase,
                 GamePhase.DECLARE_WINNER: self.declare_winner_phase,
             }
             current_phase = phases[current_phase]()
+
+    def begin_round_phase(self) -> GamePhase:
+        player_count = self.request_player_count()
+        all_cards = Card.all_cards(None)
+        cards = [Deck.draw_card(all_cards) for _ in range(player_count + 1)]
+        self.gamestate.deal_cards(all_cards, self.gamestate.players + 1)
+        return GamePhase.CHOOSE_CARD
 
     def choose_card_phase(self) -> GamePhase:
         pass
 
     def next_player_phase(self) -> GamePhase:
-        if GameState.round == 6:
+        if GameState.iround == GameState.MAX_ROUND:
             return GamePhase.DECLARE_WINNER
         self.gamestate.next_player()
         print(f"=== {self.gamestate.current_player()}'s turn")
         return GamePhase.CHOOSE_CARD
+
+    def end_round_phase(self) -> GamePhase:
+        pass
 
     def declare_winner_phase(self) -> GamePhase:
         max_score = 0
@@ -68,18 +86,37 @@ class GameServer:
         while True:
             try:
                 count = int(input("Введите количество игроков: "))
-                if 2 <= count <= 6:
+                print(f'Ввели {count}')
+                if GameState.MIN_PLAYERS <= count <= GameState.MAX_PLAYERS:
                     return count
             except ValueError:
                 pass
-            print("Пожалуйста, введите корректное число.")
+            print("Пожалуйста, введите число от 2 до 6")
 
+    @staticmethod
+    def request_player() -> (str, str):
+        """Возвращает имя и тип игрока"""
 
+        while True:
+            name = input("Введите имя игрока ")
+            if name.isalpha():
+                break
+            print("Имя должно быть одним словом, только алфавитные символы")
+
+        while True:
+            kind = input("Какой тип игрока? (введите 'human' или 'bot') ")
+            if kind.lower() in ['human', 'bot']:  # Проверка на корректный ввод типа игрока
+                break
+            print("Допустимые типы игроков: 'human' или 'bot'")
+
+        return name, kind
 def __main__():
-    server = GameServer({}, None)
-    server.new_game()
+    player_types = GameServer.get_players()
+    server = GameServer(player_types, gamestate=None)
+    server.new_game(player_types)
     server.run()
 
 
 if __name__ == "__main__":
     __main__()
+
